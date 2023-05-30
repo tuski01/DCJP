1886047b916f29c1433c2752846af2a7685f1fd3
king <tuski0@g.shingu.ac.kr>
2023-05-30 14:13:18 +0900

    MainActivity commit

--- app/src/main/java/net/flow9/dcjt/firebase/MainActivity.java
+++ app/src/main/java/net/flow9/dcjt/firebase/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
 
+
                 finish();
             }
         });
